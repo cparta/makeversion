567251981e5da6467cdba7398bc84ed70cf662d0
Johan Lindh <jli@cparta.se>
2022-04-25 11:17:57 +0200

    allow running outside git repo

--- gitter_test.go
+++ gitter_test.go
@@ -1,15 +1,49 @@
 package makeversion
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_NewDefaultGitter_SuccedsNormally(t *testing.T) {
+func Test_NewDefaultGitter_SucceedsNormally(t *testing.T) {
 	dg, err := NewDefaultGitter("git")
 	assert.NoError(t, err)
 	assert.NotNil(t, dg)
+}
+
+func Test_CheckGitRepo_SuceedsForCurrent(t *testing.T) {
+	repo, err := CheckGitRepo(".")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, repo)
+}
+
+func Test_CheckGitRepo_SuceedsForCmdMkver(t *testing.T) {
+	repo, err := CheckGitRepo("./cmd/mkver")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, repo)
+}
+
+func Test_CheckGitRepo_FailsForRoot(t *testing.T) {
+	repo, err := CheckGitRepo("/")
+	assert.Error(t, err)
+	assert.Empty(t, repo)
+}
+
+func Test_CheckGitRepo_IgnoresFileNamedGit(t *testing.T) {
+	const fileNamedGit = "./cmd/mkver/.git"
+	if _, err := os.Stat(fileNamedGit); err != nil {
+		if f, err := os.Create(fileNamedGit); err == nil {
+			defer f.Close()
+			defer os.Remove(fileNamedGit)
+			repo, err := CheckGitRepo("./cmd/mkver")
+			assert.NoError(t, err)
+			assert.NotEmpty(t, repo)
+		}
+	} else {
+		t.Logf("warning: '%s' already exists\n", fileNamedGit)
+	}
 }
 
 func Test_DefaultGitter_GetTag(t *testing.T) {
@@ -24,7 +58,7 @@ func Test_DefaultGitter_GetBranch(t *testing.T) {
 	dg, err := NewDefaultGitter("git")
 	if assert.NoError(t, err) && assert.NotNil(t, dg) {
 		assert.NotEmpty(t, dg.GetBranch("."))
-		assert.Equal(t, "HEAD", dg.GetBranch("/"))
+		assert.Equal(t, "", dg.GetBranch("/"))
 	}
 }
 
