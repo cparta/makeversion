f5846a2750bf3724a4fe4d0376e4fa19c24074e9
Johan Lindh <jli@cparta.se>
2022-06-01 08:23:47 +0200

    stretchr/testify -> matryer/is

--- versionstringer_test.go
+++ versionstringer_test.go
@@ -5,7 +5,7 @@ package makeversion
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/matryer/is"
 )
 
 type MockEnvironment map[string]string
@@ -34,18 +34,21 @@ func (mg MockGitter) GetBuild(repo string) string {
 }
 
 func Test_NewVersionStringer_SucceedsNormally(t *testing.T) {
+	is := is.New(t)
 	vs, err := NewVersionStringer("git")
-	assert.NoError(t, err)
-	assert.NotNil(t, vs)
+	is.NoErr(err)
+	is.True(vs != nil)
 }
 
 func Test_NewVersionStringer_FailsWithBadBinary(t *testing.T) {
+	is := is.New(t)
 	vs, err := NewVersionStringer("./versionstringer.go")
-	assert.Error(t, err)
-	assert.Nil(t, vs)
+	is.True(err != nil)
+	is.Equal(vs, nil)
 }
 
 func Test_VersionStringer_IsEnvTrue(t *testing.T) {
+	is := is.New(t)
 	vs := VersionStringer{
 		Env: MockEnvironment{
 			"TEST_EMPTY":      "",
@@ -54,141 +57,147 @@ func Test_VersionStringer_IsEnvTrue(t *testing.T) {
 			"TEST_TRUE_UPPER": "TRUE",
 		},
 	}
-	assert.False(t, vs.IsEnvTrue("TEST_MISSING"))
-	assert.False(t, vs.IsEnvTrue("TEST_EMPTY"))
-	assert.False(t, vs.IsEnvTrue("TEST_FALSE"))
-	assert.True(t, vs.IsEnvTrue("TEST_TRUE_LOWER"))
-	assert.True(t, vs.IsEnvTrue("TEST_TRUE_UPPER"))
+	is.Equal(vs.IsEnvTrue("TEST_MISSING"), false)
+	is.Equal(vs.IsEnvTrue("TEST_MISSING"), false)
+	is.Equal(vs.IsEnvTrue("TEST_EMPTY"), false)
+	is.Equal(vs.IsEnvTrue("TEST_FALSE"), false)
+	is.Equal(vs.IsEnvTrue("TEST_TRUE_LOWER"), true)
+	is.Equal(vs.IsEnvTrue("TEST_TRUE_UPPER"), true)
 }
 
 func Test_VersionStringer_IsReleaseBranch(t *testing.T) {
+	is := is.New(t)
 	const branchName = "testbranch"
 	env := MockEnvironment{}
 	vs := VersionStringer{Env: env}
 
-	assert.True(t, vs.IsReleaseBranch("default"))
-	assert.True(t, vs.IsReleaseBranch("main"))
-	assert.True(t, vs.IsReleaseBranch("master"))
-	assert.False(t, vs.IsReleaseBranch(branchName))
+	is.True(vs.IsReleaseBranch("default"))
+	is.True(vs.IsReleaseBranch("main"))
+	is.True(vs.IsReleaseBranch("master"))
+	is.True(!vs.IsReleaseBranch(branchName))
 
 	env["CI_DEFAULT_BRANCH"] = branchName
-	assert.True(t, vs.IsReleaseBranch(branchName))
+	is.True(vs.IsReleaseBranch(branchName))
 	delete(env, "CI_DEFAULT_BRANCH")
 
 	env["CI_COMMIT_REF_PROTECTED"] = "true"
-	assert.True(t, vs.IsReleaseBranch(branchName))
+	is.True(vs.IsReleaseBranch(branchName))
 	delete(env, "CI_COMMIT_REF_PROTECTED")
 
 	env["GITHUB_REF_PROTECTED"] = "true"
-	assert.True(t, vs.IsReleaseBranch(branchName))
+	is.True(vs.IsReleaseBranch(branchName))
 	delete(env, "GITHUB_REF_PROTECTED")
 
-	assert.False(t, vs.IsReleaseBranch(branchName))
+	is.True(!vs.IsReleaseBranch(branchName))
 }
 
 func Test_VersionStringer_GetTag(t *testing.T) {
+	is := is.New(t)
 	env := MockEnvironment{}
 	git := MockGitter{}
 	vs := VersionStringer{Git: git, Env: env}
 
 	tag, err := vs.GetTag("")
-	assert.NoError(t, err)
-	assert.Equal(t, "v0.0.0", tag)
+	is.NoErr(err)
+	is.Equal("v0.0.0", tag)
 
 	tag, err = vs.GetTag("v1.2.3")
-	assert.NoError(t, err)
-	assert.Equal(t, "v1.2.3", tag)
+	is.NoErr(err)
+	is.Equal("v1.2.3", tag)
 
 	tag, err = vs.GetTag("foo")
-	assert.Error(t, err)
-	assert.Equal(t, "foo", tag)
+	is.True(err != nil)
+	is.Equal("foo", tag)
 
 	env["CI_COMMIT_TAG"] = "v3"
 	tag, err = vs.GetTag("")
-	assert.NoError(t, err)
-	assert.Equal(t, "v3", tag)
+	is.NoErr(err)
+	is.Equal("v3", tag)
 }
 
 func Test_VersionStringer_GetBranch(t *testing.T) {
+	is := is.New(t)
 	env := MockEnvironment{}
 	git := MockGitter{}
 	vs := VersionStringer{Git: git, Env: env}
 
 	text, name := vs.GetBranch("branch")
-	assert.Equal(t, "branch", name)
-	assert.Equal(t, "branch", text)
+	is.Equal("branch", name)
+	is.Equal("branch", text)
 
 	text, name = vs.GetBranch("branch.with..dots")
-	assert.Equal(t, "branch.with..dots", name)
-	assert.Equal(t, "branch-with-dots", text)
+	is.Equal("branch.with..dots", name)
+	is.Equal("branch-with-dots", text)
 
 	env["CI_COMMIT_REF_NAME"] = "gitlab-branch"
 	text, name = vs.GetBranch("")
-	assert.Equal(t, "gitlab-branch", name)
-	assert.Equal(t, "gitlab-branch", text)
+	is.Equal("gitlab-branch", name)
+	is.Equal("gitlab-branch", text)
 	delete(env, "CI_COMMIT_REF_NAME")
 
 	env["GITHUB_REF_NAME"] = "github.branch"
 	text, name = vs.GetBranch("")
-	assert.Equal(t, "github.branch", name)
-	assert.Equal(t, "github-branch", text)
+	is.Equal("github.branch", name)
+	is.Equal("github-branch", text)
 	delete(env, "GITHUB_REF_NAME")
 }
 
 func Test_VersionStringer_GetBuild(t *testing.T) {
+	is := is.New(t)
 	env := MockEnvironment{}
 	git := MockGitter{}
 	vs := VersionStringer{Git: git, Env: env}
 
 	build := vs.GetBuild("123")
-	assert.Equal(t, "123", build)
+	is.Equal("123", build)
 
 	env["CI_PIPELINE_IID"] = "456"
 	build = vs.GetBuild("")
-	assert.Equal(t, "456", build)
+	is.Equal("456", build)
 	delete(env, "CI_PIPELINE_IID")
 
 	env["GITHUB_RUN_NUMBER"] = "789"
 	build = vs.GetBuild("345")
-	assert.Equal(t, "789", build)
+	is.Equal("789", build)
 	delete(env, "CI_PIPELINE_IID")
 }
 
 func Test_VersionStringer_GetVersion(t *testing.T) {
+	is := is.New(t)
 	env := MockEnvironment{}
 	git := MockGitter{}
 	vs := VersionStringer{Git: git, Env: env}
 
 	vi, err := vs.GetVersion("v1", false)
-	assert.NoError(t, err)
-	assert.Equal(t, "v1-v1.v1", vi.Version)
+	is.NoErr(err)
+	is.Equal("v1-v1.v1", vi.Version)
 
 	vi, err = vs.GetVersion("", false)
-	assert.NoError(t, err)
-	assert.Equal(t, "v0.0.0", vi.Version)
+	is.NoErr(err)
+	is.Equal("v0.0.0", vi.Version)
 
 	env["CI_COMMIT_REF_NAME"] = "HEAD"
 	vi, err = vs.GetVersion("", false)
-	assert.NoError(t, err)
-	assert.Equal(t, "v0.0.0-HEAD", vi.Version)
+	is.NoErr(err)
+	is.Equal("v0.0.0-HEAD", vi.Version)
 
 	delete(env, "CI_COMMIT_REF_NAME")
 	env["GITHUB_RUN_NUMBER"] = "789"
 	vi, err = vs.GetVersion("", false)
-	assert.NoError(t, err)
-	assert.Equal(t, "v0.0.0-789", vi.Version)
+	is.NoErr(err)
+	is.Equal("v0.0.0-789", vi.Version)
 
 	env["CI_COMMIT_REF_NAME"] = "*Branch--.--ONE*-*"
 	env["GITHUB_RUN_NUMBER"] = "789"
 	vi, err = vs.GetVersion("v2.0", false)
-	assert.NoError(t, err)
-	assert.Equal(t, "v2.0-branch-one.789", vi.Version)
+	is.NoErr(err)
+	is.Equal("v2.0-branch-one.789", vi.Version)
 
 	vi, err = vs.GetVersion("v3.4.5", true)
-	assert.Error(t, err)
+	is.True(err != nil)
 
 	env["CI_COMMIT_REF_NAME"] = "main"
 	vi, err = vs.GetVersion("v3.4.5", true)
-	assert.NoError(t, err)
-	assert.Equal(t, "v3.4.5", vi.Version)
+	is.NoErr(err)
+	is.Equal("v3.4.5", vi.Version)
 }
